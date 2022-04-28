dcafc4561eddf49b21f268c1cb051284c747eeb0
digitbuzz <lxtanner@yahoo.com>
2022-04-28 11:37:21 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Alexander Tanner on 28/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
